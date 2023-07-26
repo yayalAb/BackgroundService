595738988e2d1f6848c2fae689a06d33c52a4bbc
YayalAb <yayalabayneh2@gmail.com>
2023-07-26 17:10:25 +0300

    fix: finger print reader cors error

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ namespace MyBackgroundService
 		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 		{
 			listener = new HttpListener();
-			listener.Prefixes.Add("http://localhost:8000/");
+			listener.Prefixes.Add("http://192.168.1.16:8000/");
 			listener.Start();
 			Console.WriteLine("Listener started ");
 			try
@@ -59,18 +59,33 @@ namespace MyBackgroundService
 			listener.Stop();
 		}
 
-		private void ProcessRequest(HttpListenerContext context)
+		private async void ProcessRequest(HttpListenerContext context)
 		{
 			AddFingerprint fp = new AddFingerprint();
-			
+			string accessToken = "";
+			string authorizationHeader = context.Request.Headers["Authorization"];
+			  if (!string.IsNullOrEmpty(authorizationHeader) && authorizationHeader.StartsWith("Bearer "))
+				{
+					 accessToken = authorizationHeader.Substring("Bearer ".Length);
+				}
+				
 			string clientAddress = context.Request.RemoteEndPoint.ToString();
-			Console.WriteLine($"Client connected: {clientAddress}");
-
-			// Set CORS headers
 			context.Response.AddHeader("Access-Control-Allow-Origin", "*");
-
-			// Set content type to JSON
-			context.Response.ContentType = "application/json";
+			context.Response.ContentType = "application/json, text/plain, */*";
+			context.Response.AddHeader("Access-Control-Allow-Methods", "GET");
+			context.Response.AddHeader("Access-Control-Allow-Headers", "*");
+			context.Response.AddHeader("lang","en");
+			context.Response.AddHeader("Authorization",accessToken);
+			if (context.Request.HttpMethod == "OPTIONS")
+			{
+				Console
+				.WriteLine("It is perfilght request ");
+			}else
+			{
+				 Console
+                .WriteLine("It is perfilght a request option");
+			}
+			context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
 
 			// Create and serialize response message
 			 var response = new Response
@@ -84,15 +99,49 @@ namespace MyBackgroundService
 
 
 			// Write response to output stream
+		int retries = 3;
+			bool success = false;
+			while (!success && retries > 0)
+			{
+				try
+				{
 			byte[] buffer = Encoding.UTF8.GetBytes(json);
-			context.Response.OutputStream.Write(buffer, 0, buffer.Length);
-
+		
+			await context.Response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
 			// Close output stream and context
 			context.Response.OutputStream.Close();
 			context.Response.Close();
+			 success = true;
+	   }
+	 catch (IOException ex)
+		{
+			Console.WriteLine($"IO Exception occurred: {ex.Message}");
+		}
+	catch (HttpListenerException ex)
+	{
+		if (ex.ErrorCode == 64)
+		{
+				Console.WriteLine("Network name is no longer available");
+				retries--;
+			if (retries == 0)
+			{
+				throw new Exception("Network name is no longer available!");
+			}
+			else
+			{
+				System.Threading.Thread.Sleep(1000);
+			}
+		}
+		else
+		{
+			throw;
+		}
+	}finally
+	{
+			Console.WriteLine($"Client disconnected: {clientAddress}");	
+	}
+}
 
-			Console.WriteLine($"Sent JSON response to {clientAddress}");
-			Console.WriteLine($"Client disconnected: {clientAddress}");
 		}
    
 	
@@ -113,15 +162,14 @@ namespace MyBackgroundService
 				device.StartFingerDetection();
 				Output.WriteLine("Please place your finger on the device or press enter to cancel");
 		   if (fingerprintDetectedEvent.WaitOne(10000)) {
-					 Output.WriteLine("Please place your finger on the device...nbnbnv");
+					 Output.WriteLine("Please place your finger on the device");
 			   } 
 		   else {
-			 Output.WriteLine("Please place your finger on the device... time out");
+			 Output.WriteLine("Connection time out");
 			 }
 				device.Dispose();
 		 return FingerPrint;
 							}
-
 			private string ? HandleNewFingerprint(Bitmap bitmap)
 			{
 				byte[] imageData;
@@ -130,7 +178,7 @@ namespace MyBackgroundService
 				{
 					bitmap.Save(ms, ImageFormat.Png); // save the bitmap to a memory stream as a PNG image
 					imageData = ms.ToArray(); // get the bytes from the memory stream
-				    base64String = Convert.ToBase64String(imageData);
+					base64String = Convert.ToBase64String(imageData);
 				}
 				 Output.WriteLine(ConsoleColor.DarkGreen, "Fingerprint registered");
 			   return base64String;
